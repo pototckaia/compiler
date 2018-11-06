d010d1699272deaa4fe4784017d52ed0660a0e22
pototckaia <pototckaia@gmail.com>
2018-11-06 22:29:20 +1000

    Add сommand line parameters

--- main.cpp
+++ main.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 #include "lexer.h"
 #include "lexer_exception.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+  std::string inputFileName, outputFileName;
 
-  std::string file_name = "input.txt";;
-  std::ofstream wfile;
-  wfile.open("out.txt", std::ifstream::out);
+  if (argc < 2 || argc > 5 || argc == 3) {
+    std::cerr << "Error in params" << std::endl;
+    return 0;
+  }
 
-  Lexer l(file_name);
+  if (argc == 2) {
+    inputFileName = argv[1];
+    outputFileName = inputFileName;
 
+    auto typeFile = std::find_if(outputFileName.rbegin(), outputFileName.rend(), [](int ch){ return ch == '.'; });
+    outputFileName.erase(typeFile.base() - 1, outputFileName.end());
+    outputFileName += ".out";
+  } else if (argv[1][0] == '-' || argv[2][0] == '-') {
+    int inIndex = 1, outIndex = 3;
 
-  while(!l.is_end()) {
-    try {
-      auto token = l.next();
-      if (token->token_type_ == tok::TokenType::Comment) {
-        continue;
-      }
-      wfile << token->to_string() << std::endl;
-    } catch (std::exception &e) {
-      wfile << e.what() << std::endl;
+    if (argv[1][0] == '-') {
+      inIndex = 3, outIndex = 2;
     }
+
+    inputFileName = argv[inIndex];
+    outputFileName = argv[outIndex];
+  } else {
+    std::cerr << "Error in params" << std::endl;
+    return 0;
+  }
+
+
+  std::ofstream wfile;
+  wfile.open(outputFileName, std::ifstream::out);
+
+  Lexer lex(inputFileName);
+
+  try {
+    for (auto token = lex.next(); token != nullptr; token = lex.next()) {
+      wfile << token->toString() << std::endl;
+    }
+  } catch(lxerr::LexerException& e) {
+    wfile << e.what() << std::endl;
   }
 
   wfile.close();
