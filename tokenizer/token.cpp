eeb34a8b4ea7fc6592b9d284b1b3747f27e3ca83
Anastasia <pototckaia@gmail.com>
2018-11-15 01:12:48 +1000

    Add get function and refactor

--- tokenizer/token.cpp
+++ tokenizer/token.cpp
@@ -2,29 +2,31 @@
 
 #include <string>
 
-TokenBase::TokenBase(int line, int column, tok::TokenType tokenType, const std::string &strValue)
-    : line(line), column(column),
-      tokenType(tokenType),
-      strValue(strValue) {}
+using namespace tok;
+
+TokenBase::TokenBase(int line, int column, tok::TokenType tokenType, const std::string& strValue)
+  : line(line), column(column),
+    tokenType(tokenType),
+    strValue(strValue) {}
 
 std::string TokenBase::toString() {
   return std::to_string(line) + "\t" + std::to_string(column) + "\t" +
          tok::toString(tokenType) + "\t\"" + strValue + "\"\t";
 }
 
-template <typename T>
+template<typename T>
 Token<T>::Token(int line, int column, tok::TokenType tokenType,
-                T value, const std::string &strValue)
-    : TokenBase(line, column, tokenType, strValue), value(value) {}
+                T value, const std::string& strValue)
+  : TokenBase(line, column, tokenType, strValue), value(value) {}
 
-template <typename T>
+template<typename T>
 std::string Token<T>::toString() {
   return TokenBase::toString() + "\t" + std::to_string(value);
 }
 
 Token<std::string>::Token(int line, int column, tok::TokenType tokenType,
-                          const std::string& value, const std::string &str_value)
-    : TokenBase(line, column, tokenType, str_value), value(value) {}
+                          const std::string& value, const std::string& str_value)
+  : TokenBase(line, column, tokenType, str_value), value(value) {}
 
 std::string Token<std::string>::toString() {
   return TokenBase::toString() + "\t\"" + value + "\"";
@@ -32,12 +34,15 @@ std::string Token<std::string>::toString() {
 
 
 Token<tok::KeywordType>::Token(int line, int column, tok::TokenType tokenType,
-                               tok::KeywordType value, const std::string &str_value)
-    : TokenBase(line, column, tokenType, str_value), value(value) {}
+                               tok::KeywordType value, const std::string& str_value)
+  : TokenBase(line, column, tokenType, str_value), value(value) {}
 
 std::string Token<tok::KeywordType>::toString() {
   return TokenBase::toString() + "\t" + tok::toString(value);
 }
 
-template class Token<long long>;
-template class Token<long double>;
+template
+class Token<long long>;
+
+template
+class Token<long double>;
--- tokenizer/token.h
+++ tokenizer/token.h
@@ -4,14 +4,21 @@
 
 #include "token_type.h"
 
+namespace tok {
 
 class TokenBase {
  public:
   TokenBase() = delete;
+
   TokenBase(int line, int column, tok::TokenType token_type, const std::string& strValue);
+
   virtual ~TokenBase() = default;
 
   virtual std::string toString();
+
+  tok::TokenType getTokenType() { return tokenType; }
+
+  virtual std::string getValueString() = 0;
 
  private:
   int line, column;
@@ -19,14 +26,18 @@ class TokenBase {
   std::string strValue;
 };
 
-template <typename T>
+template<typename T>
 class Token : public TokenBase {
  public:
   Token() = delete;
+
   Token(int line, int column, tok::TokenType token_type, T value, const std::string& str_value);
+
   ~Token() override = default;
 
   std::string toString() override;
+
+  std::string getValueString() override { return std::to_string(value); }
 
  private:
   T value;
@@ -36,11 +47,15 @@ template<>
 class Token<std::string> : public TokenBase {
  public:
   Token() = delete;
+
   Token(int line, int column, tok::TokenType tokenType,
-                     const std::string& value, const std::string& str_value);
+        const std::string& value, const std::string& str_value);
+
   ~Token() override = default;
 
   std::string toString() override;
+
+  std::string getValueString() override { return value; }
 
  private:
   std::string value;
@@ -50,12 +65,18 @@ template<>
 class Token<tok::KeywordType> : public TokenBase {
  public:
   Token() = delete;
+
   Token(int line, int column, tok::TokenType tokenType,
         tok::KeywordType value, const std::string& str_value);
+
   ~Token() override = default;
 
   std::string toString() override;
 
+  std::string getValueString() override { return tok::toString(value); };
+
  private:
   tok::KeywordType value;
 };
+
+} // namespace tok
