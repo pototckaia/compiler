03e92c0937b7b6f2361997cd42a753cbd8110741
Nastya <pototckaia@gmail.com>
2018-11-23 12:37:45 +1000

    Add two constructor to ParserException

--- exception.h
+++ exception.h
@@ -17,9 +17,13 @@ class LexerException : public std::exception {
 
 class ParserException : public std::exception {
  public:
-  explicit ParserException(const std::string& ss) : s(ss) {}
-  explicit ParserException(int line, int column, const std::string& ss) : s(std::to_string(line) + ", " +
-                                                                            std::to_string(column) + " " + ss) {}
+  explicit ParserException(int line, int column)
+    : s(std::to_string(line) + "," + std::to_string(column) + "\t Illegal expression") {}
+
+  explicit ParserException(int line, int column, tok::TokenType exceptType, tok::TokenType getType)
+    : s(std::to_string(line) + "," + std::to_string(column) + "\t" +
+        "Except: \""  + tok::toString(exceptType) + "\" but find \"" +
+        tok::toString(getType)) {}
   ~ParserException() override = default;
 
   const char* what() const noexcept { return s.c_str(); }
