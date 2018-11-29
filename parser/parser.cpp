7fe9d95f16c8dd4afe05486443c80b92edaf5500
Nastya <pototckaia@gmail.com>
2018-11-29 10:22:41 +1000

    Add parseAccess

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -3,7 +3,8 @@
 
 using namespace pr;
 
-Parser::Parser(const std::string& s) : lexer(s) {
+Parser::Parser(const std::string& s)
+  : lexer(s) {
   priority[0] = {tok::TokenType::StrictLess, tok::TokenType::StrictGreater,
                  tok::TokenType::NotEquals, tok::TokenType::Equals,
                  tok::TokenType::LessOrEquals, tok::TokenType::GreaterOrEquals};
@@ -14,65 +15,128 @@ Parser::Parser(const std::string& s) : lexer(s) {
                  tok::TokenType::And,
                  tok::TokenType::Shr, tok::TokenType::ShiftRight, // TODO: replace one tokenType
                  tok::TokenType::Shl, tok::TokenType::ShiftLeft};
+  priority[3] = {tok::TokenType::Not, tok::TokenType::Minus,
+                 tok::TokenType::Plus, tok::TokenType::At};
+  priorityLeftUnary  = 3;
+  priority[4] = {tok::TokenType::Caret, tok::TokenType::Dot,
+                 tok::TokenType::OpenSquareBracket,
+                 tok::TokenType::OpenParenthesis};
+  priorityAccess = 4;
 }
 
-std::unique_ptr<ASTNode> Parser::parseFactor() {
-  notRequire(tok::TokenType::EndOfFile);
-  auto token = lexer.next();
 
-  switch (token->getTokenType()) {
-    case tok::TokenType::Not:
-    case tok::TokenType::Minus:
-    case tok::TokenType::Plus:
-    case tok::TokenType::At: {
-      return std::make_unique<UnaryOperation>(std::move(token), parseFactor());
-    }
+ListExpr Parser::parseListExpression() {
+  ListExpr list;
+  list.push_back(parseBinaryOperator());
+  while(lexer.get()->getTokenType() == tok::TokenType::Comma) {
+    lexer.next();
+    list.push_back(parseBinaryOperator());
+  }
+  return list;
+}
+
+ListExpr Parser::parseActualParameter() {
+  ListExpr list;
+  if (lexer.get()->getTokenType() == tok::TokenType::CloseParenthesis) {
+    return list;
+  }
+  return parseListExpression();
+}
+
+ptr_Expr Parser::parseFactor() {
+  auto token = lexer.next();
+  auto type = token->getTokenType();
+
+  switch (type) {
     case tok::TokenType::Int:
-    case tok::TokenType::Double: {
+    case tok::TokenType::Double:
+    case tok::TokenType::Nil: {
       return std::make_unique<Literal>(std::move(token));
     }
     case tok::TokenType::Id: {
       return std::make_unique<Variable>(std::move(token));
     }
     case tok::TokenType::OpenParenthesis: {
-      auto expr = parseTerm();
-
+      auto expr = parseBinaryOperator();
       require(tok::TokenType::CloseParenthesis);
       lexer.next();
-
       return expr;
     }
     default:
-      throw ParserException(token->getLine(), token->getColumn());
+      throw ParserException(token->getLine(), token->getColumn(), type);
   }
 }
 
-std::unique_ptr<ASTNode> Parser::parseTerm(int p) {
-  auto left = getTermOrFactor(p + 1);
+ptr_Expr Parser::parseAccess(int p) {
+  auto left = getExprByPriority(p + 1);
 
-  while (true) {
-    if (!require(priority[p])) {
-      break;
+  while (require(priority[p])) {
+    switch (lexer.get()->getTokenType()) {
+      case tok::TokenType::Dot: {
+        lexer.next();
+        require(tok::TokenType::Id);
+        left = std::make_unique<RecordAccess>(std::move(left), lexer.next());
+        break;
+      }
+      case tok::TokenType::OpenParenthesis: {
+        lexer.next();
+        auto list = parseActualParameter();
+        require(tok::TokenType::CloseParenthesis);
+        lexer.next();
+        left = std::make_unique<FunctionCall>(std::move(left), std::move(list));
+        break;
+      }
+      case tok::TokenType::OpenSquareBracket: {
+        lexer.next();
+        auto list = parseListExpression();
+        require(tok::TokenType::CloseSquareBracket);
+        lexer.next();
+        left = std::make_unique<ArrayAccess>(std::move(left), std::move(list));
+        break;
+      }
+      case tok::TokenType::Caret: {
+        left = std::make_unique<UnaryOperation>(lexer.next(), std::move(left));
+        break;
+      }
     }
-    auto op = lexer.next();
-    auto right = getTermOrFactor(p + 1);
-
-    left = std::make_unique<BinaryOperation>(std::move(op), std::move(left), std::move(right));
   }
 
   return left;
 }
 
-std::unique_ptr<pr::ASTNode> Parser::getTermOrFactor(int p) {
+ptr_Expr Parser::parseUnaryOperator(int p) {
+  if (!require(priority[p])) {
+      return getExprByPriority(p + 1);
+  }
+  auto op = lexer.next();
+  return std::make_unique<UnaryOperation>(std::move(op), parseUnaryOperator(p));
+}
+
+ptr_Expr Parser::parseBinaryOperator(int p) {
+  auto left = getExprByPriority(p + 1);
+
+  while (require(priority[p])) {
+    auto op = lexer.next();
+    auto right = getExprByPriority(p + 1);
+    left = std::make_unique<BinaryOperation>(std::move(op), std::move(left), std::move(right));
+  }
+  return left;
+}
+
+ptr_Expr Parser::getExprByPriority(int p) {
   if (p == priority.size()) {
     return parseFactor();
+  } else if (p == priorityLeftUnary) {
+    return parseUnaryOperator(p);
+  } else if (p == priorityAccess) {
+    return parseAccess(p);
   } else {
-    return parseTerm(p);
+    return parseBinaryOperator(p);
   }
 }
 
 std::unique_ptr<pr::ASTNode> Parser::parse() {
-  return getTermOrFactor();
+  return getExprByPriority();
 }
 
 bool Parser::require(std::list<tok::TokenType>& listType) {
@@ -94,6 +158,6 @@ void Parser::require(tok::TokenType type) {
 void Parser::notRequire(tok::TokenType type) {
   auto& get = lexer.get();
   if (get->getTokenType() == type) {
-    throw ParserException(get->getLine(), get->getColumn());
+    throw ParserException(get->getLine(), get->getColumn(), type);
   }
 }
--- parser/parser.h
+++ parser/parser.h
@@ -5,20 +5,30 @@
 
 namespace pr {
 
+using ptr_Token = std::unique_ptr<tok::TokenBase>;
+
 class Parser {
  public:
   explicit Parser(const std::string&);
 
-  std::unique_ptr<pr::ASTNode> parse();
+  ptr_Expr parse();
 
  private:
   lx::LexerBuffer lexer;
   // increasing priority for binary operator
-  std::array<std::list<tok::TokenType>, 3> priority;
+  std::array<std::list<tok::TokenType>, 5> priority;
+  int priorityLeftUnary;
+  int priorityAccess;
 
-  std::unique_ptr<pr::ASTNode> parseFactor();
-  std::unique_ptr<pr::ASTNode> parseTerm(int priority = 0);
-  std::unique_ptr<pr::ASTNode> getTermOrFactor(int p = 0);
+  ListExpr parseListExpression();
+  ListExpr parseActualParameter();
+
+  ptr_Expr parseFactor();
+  ptr_Expr parseAccess(int p);
+  ptr_Expr parseUnaryOperator(int p);
+  ptr_Expr parseBinaryOperator(int priority = 0);
+
+  ptr_Expr getExprByPriority(int p = 0);
 
   void require(tok::TokenType);
   void notRequire(tok::TokenType);
