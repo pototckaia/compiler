f2cdcb20323ca6cf15512c44899222ef202ca995
Anastasia <pototckaia@gmail.com>
2018-12-20 17:20:30 +1000

    Add TypeCheck exceptions

--- exception.h
+++ exception.h
@@ -2,6 +2,9 @@
 
 #include <string>
 
+#include "token.h"
+
+
 class LexerException : public std::exception {
  public:
   explicit LexerException(const std::string& ss) : s(ss) {}
@@ -18,14 +21,14 @@ class LexerException : public std::exception {
 class ParserException : public std::exception {
  public:
   explicit ParserException(int line, int column, std::string s)
-    : s(std::to_string(line) + "," + std::to_string(column) + "\t Illegal expression: " + s) {}
+    : s(tok::getPoint(line, column) + " Illegal expression: " + s) {}
 
   explicit ParserException(int line, int column, tok::TokenType t)
     : ParserException(line, column, tok::toString(t)) {}
 
   explicit ParserException(int line, int column, std::string except, std::string get)
-    : s(std::to_string(line) + "," + std::to_string(column) + "\t" +
-        "Except: \""  + except + "\" but find \"" + get + "\"") {}
+    : s(tok::getPoint(line, column) + "Except: \""  + except +
+        "\" but find \"" + get + "\"") {}
 
   explicit ParserException(int line, int column, tok::TokenType exceptType, tok::TokenType getType)
     : ParserException(line, column, tok::toString(exceptType), tok::toString(getType)) {}
@@ -34,6 +37,48 @@ class ParserException : public std::exception {
 
   const char* what() const noexcept { return s.c_str(); }
 
+ private:
+  std::string s;
+};
+
+class AlreadyDefinedException : public std::exception {
+ public:
+  explicit AlreadyDefinedException(std::string n) : s(a + "\"" + n + "\"") {};
+  explicit AlreadyDefinedException(int line, int column, std::string n)
+    : s(tok::getPoint(line, column) + a + "\"" + n + "\"") {}
+  explicit AlreadyDefinedException(const tok::ptr_Token& t)
+    : s(getPoint(t) + a + "\"" + t->getValueString() + "\"") {}
+
+  ~AlreadyDefinedException() override = default;
+  const char* what() const noexcept { return s.c_str(); }
+
+ private:
+  const std::string a = "Already defined ";
+  std::string s;
+};
+
+class NotDefinedException: public std::exception {
+ public:
+  explicit NotDefinedException(const std::string& s)
+   : s(a + "\"" + s + "\"") {};
+
+  explicit NotDefinedException(const tok::ptr_Token& t)
+   : s(getPoint(t) + a + "\"" + t->getValueString() + "\"") {};
+
+  ~NotDefinedException() override = default;
+  const char* what() const noexcept { return s.c_str(); }
+
+ private:
+  const std::string a = "Not defined ";
+  std::string s;
+};
+
+class SemanticException : public std::exception {
+ public:
+  explicit SemanticException(int line, int column, std::string n)
+    : s(tok::getPoint(line, column) + " " + n) {}
+
+  const char* what() const noexcept { return s.c_str(); }
  private:
   std::string s;
 };
